504cfdefe9bc2682696bbf415872b59fc6fdec52
tallskys <tallsky89@gmail.com>
2020-04-12 09:40:02 -0400

    remove codepen link

--- config.js
+++ config.js
@@ -35,7 +35,7 @@ module.exports = {
       line: '',
       gitlab: '',
       weibo: '',
-      codepen: 'https://codepen.io/tcarrico',
+      codepen: '',
       youtube: '',
       soundcloud: '',
     }
